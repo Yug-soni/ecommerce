d90ff82068e39d8e770d9c1ba03d7d6a518aab9e
Yug-soni <yugsoni8946842393@gmail.com>
2021-06-30 14:45:08 +0530

    nothing new

---
+++ src/components/logo/logo.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logo from "./../../assets/logo.jpeg";
+
+import "./logo.styles.scss";
+
+const Logo = () => (
+  <div className="logo">
+    <img src={logo} alt="ECommerce" />
+  </div>
+);
+
+export default Logo;
