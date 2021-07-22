95dd0f607e81dc4274bcf85d077770e7eef0896b
Yug-soni <yugsoni8946842393@gmail.com>
2021-07-22 16:00:50 +0530

    starting to replace the css with styled components and many more new features.

--- src/App.js
+++ src/App.js
@@ -3,41 +3,39 @@ import Welcome from "./admin/welcome/Welcome.page";
 import Login from "./admin/Login/Login.component";
 import ProductView from "./admin/product-view/ProductView.component";
 // import Admin from "./admin/HomePage.page";
-import HomePage from "./page/home/HomePage.component";
+import HomePage from "./pages/home/HomePage.component";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      special: "special",
-      isLoggedIn: true,
-    };
-  }
+		this.state = {
+			special: "special",
+			isLoggedIn: true,
+		};
+	}
 
-  render() {
-    const { special } = this.state;
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path={`/${special}`} exact>
-            <Welcome {...this.state} />
-          </Route>
-          <Route path={`/${special}/login`} exact>
-            <Login {...this.state} />
-          </Route>
-          <Route path={`/${special}/product-v`} exact>
-            <ProductView />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+	render() {
+		const { special } = this.state;
+		return (
+			<Switch>
+				<Route path="/" exact>
+					<HomePage />
+				</Route>
+				<Route path={`/${special}`} exact>
+					<Welcome {...this.state} />
+				</Route>
+				<Route path={`/${special}/login`} exact>
+					<Login {...this.state} />
+				</Route>
+				<Route path={`/${special}/product-v`} exact>
+					<ProductView />
+				</Route>
+			</Switch>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import GlobalStyles from "./theme/GlobalStyles";
 import Theme from "./theme/Theme";
 
 ReactDOM.render(
-	<>
+	<Router>
 		<GlobalStyles theme={Theme} />
 		<App />
-	</>,
+	</Router>,
 	document.getElementById("root")
 );
